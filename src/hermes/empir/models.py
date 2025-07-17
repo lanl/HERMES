9f154fa36060243acc95b94f255772194dd4bb51
Alexander M. Long <alexlong@lanl.gov>
2025-07-16 20:56:18 -0600

    Modified Models.py

--- src/hermes/empir/models.py
+++ src/hermes/empir/models.py
@@ -4,6 +4,7 @@ import struct
 
 class DirectoryStructure(BaseModel):
     destination_dir: str = Field(..., description="Location of run directory")
+    create_subdirs: bool = Field(default=True, description="Create subdirectories for different file types")
     log_file_dir: str = Field(..., description="Location of log files")
     tpx3_file_dir: str = Field(..., description="Location of TPX3 files")
     list_file_dir: str = Field(..., description="Location of list files")
@@ -12,9 +13,9 @@ class DirectoryStructure(BaseModel):
     export_file_dir: str = Field(..., description="Location of exported info files")
 
 class PixelToPhotonParams(BaseModel):
-    input_file: Optional[str] = Field(default=None, description="Path and name of the input TPX3 file")
-    output_file: Optional[str] = Field(default=None, description="Path and name of the output photon file")
-    log_file: Optional[str] = Field(default=None, description="Path to the log file")
+    input_file_name: Optional[str] = Field(default=None, description="Name of the input TPX3 file")
+    output_file_name: Optional[str] = Field(default=None, description="Name of the output photon file")
+    log_file_name: Optional[str] = Field(default=None, description="Name of the log file")
     d_space: int = Field(default=10, description="Distance in space for pixel search [px]")
     d_time: float = Field(default=1e-6, description="Distance in time for pixel search [s]")
     min_number: int = Field(default=2, description="Minimum number of pixels in a photon event")
@@ -22,8 +23,8 @@ class PixelToPhotonParams(BaseModel):
     parameter_file: Optional[str] = Field(default=None, description="Path and name of a .json file where processing parameters are defined")
 
 class PhotonToEventParams(BaseModel):
-    input_file: Optional[str] = Field(default=None, description="Path and name of the input .empirphot file")
-    output_file: Optional[str] = Field(default=None, description="Path and name of the output event file")
+    input_file: Optional[str] = Field(default=None, description="Name of the input .empirphot file")
+    output_file: Optional[str] = Field(default=None, description="Name of the output event file")
     log_file: Optional[str] = Field(default=None, description="Path to the log file")
     d_space: Optional[int] = Field(default=None, description="Distance in space for photon search [px]")
     d_time: Optional[float] = Field(default=None, description="Distance in time for photon search [s]")
@@ -33,10 +34,8 @@ class PhotonToEventParams(BaseModel):
 
 class EventToImageParams(BaseModel):
     input_files: Optional[str] = Field(default=None, description="Comma separated list of files to process")
-    input_folder: Optional[str] = Field(default=None, description="All files with the .empirevent extension directly in this folder will be processed")
-    input_list_file: Optional[str] = Field(default=None, description="Path of text file containing one input file per line")
+    input_list_file: Optional[str] = Field(default=None, description="Path and name of text file containing one input file per line")
     output_file: Optional[str] = Field(default=None, description="Path (including file name) for the output")
-    params_file: Optional[str] = Field(default=None, description="Path to a .json file where processing parameters are defined")
     size_x: int = Field(default=512, description="Number of pixels in x direction for the final image")
     size_y: int = Field(default=512, description="Number of pixels in y direction for the final image")
     nPhotons_min: int = Field(default=0, description="Minimum number of photons for image processing")
