1222f872a34e1321430ba4af4b0bd2c84359613b
Alexander M. Long <alexlong@lanl.gov>
2025-07-16 20:55:46 -0600

    modified __init__.py

--- src/hermes/empir/__init__.py
+++ src/hermes/empir/__init__.py
@@ -5,7 +5,7 @@ This package contains modules and functions related to analysis with the EMPIR c
 """
 
 # Import models
-from .models import (
+from hermes.empir.models import (
     PixelToPhotonParams,
     PhotonToEventParams,
     EventToImageParams,
@@ -15,47 +15,26 @@ from .models import (
 )
 
 # Import core functions for EMPIR analysis
-from .config import (
-    empirConfig,
+from hermes.empir.config import (
+    EmpirConfig,
     zip_file,
     check_for_files,
 )
 
 # Import export functions for EMPIR analysis
-from .export import (
+from hermes.empir.export import (
     export_pixel_activations,
     export_photons,
     read_exported_pixel_activations
 )
 
 # Import processing functions for EMPIR analysis
-from .process import (
+from hermes.empir.process import (
     process_pixels_to_photons,
     process_photons_to_events,
     process_event_files_to_image_stack
 )
 
-# Define package-level variables or functions if needed
-__all__ = [
-    'empirConfig',
-    'zip_file',
-    'check_for_files',
-    'process_pixels_to_photons',
-    'process_photons_to_events',
-    'process_event_files_to_image_stack',
-    'export_pixel_activations',
-    'export_photons',
-    'read_exported_pixel_activations',
-    'PixelToPhotonParams',
-    'PhotonToEventParams',
-    'EventToImageParams',
-    'DirectoryStructure',
-    'PixelActivations',
-    'Photons'
-]
-
 # Initialize package-level settings or configurations if needed
 def initialize():
     pass
-
-# Any other package-level code can go here
