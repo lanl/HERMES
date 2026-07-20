897d17c82973d126e28aa1ec32cf4aef24a40fcf
Alexander M. Long <alexlong@lanl.gov>
2026-07-20 15:03:06 -0600

    Refactor tpx3SpidrUnpacker.cpp to simplify command-line interface and improve error handling

--- backends/unpackers/tpx3-spidr/cpp/src/tpx3SpidrUnpacker.cpp
+++ backends/unpackers/tpx3-spidr/cpp/src/tpx3SpidrUnpacker.cpp
@@ -1,89 +1,24 @@
-/**
- * @file tpx3SpidrUnpacker.cpp
- * @brief Main entry point for the TPX3 SPIDR data unpacking application.
- *
- * This program can be used in multiple ways:
- * 1. ./tpx3SpidrUnpacker -i <input_file> [options...]
- * 2. ./tpx3SpidrUnpacker -c <config_file> [options...]
- * 3. ./tpx3SpidrUnpacker -I <input_dir> [options...]
- *
- * The flag-based interface allows for flexible configuration with the ability
- * to override config file settings via command-line flags.
- */
-
-#include <chrono>
+#include <fstream>
 #include <iostream>
 
-// HERMES defined functions 
-#include "dataPacketProcessor.h"
-#include "structures.h"
 #include "diagnostics.h"
-#include "configReader.h"
-#include "commandLineParser.h"
+#include "unpacker.h"
 
-using namespace std;
-
-int main(int argc, char *argv[]){  
-
-    configParameters configParams;
-    int helpLevel = 0;
-
-    // Check for the number of command-line arguments
-    if (argc < 2) {
-        cerr << "Error: Please provide command-line arguments." << endl;
-        printUsage(argv[0]);
-        return 1;
+int main(const int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " input.tpx3\n";
+        return 2;
     }
 
-    string firstArg = argv[1];
-    
-    // Case 1: Help requested
-    if (firstArg == "-h" || firstArg == "--help") {
-        // Check if next argument is a number for help level
-        if (argc > 2 && argv[2][0] != '-') {
-            helpLevel = parseIntOrDefault(argv[2], 1);
-            if (helpLevel < 1 || helpLevel > 2) {
-                helpLevel = 1;  // Default to level 1 if invalid
-            }
-        } else {
-            helpLevel = 1;  // Default help level
-        }
-        printUsage(argv[0], helpLevel);
-        return 0;
-    }
-    
-    // Case 2: Flag-based parsing (starts with -)
-    else if (firstArg[0] == '-') {
-        if (!parseCommandLineFlags(argc, argv, configParams, helpLevel)) {
-            if (helpLevel > 0) {
-                // Help was requested during parsing
-                printUsage(argv[0], helpLevel);
-            } else {
-                // Error occurred
-                printUsage(argv[0]);
-            }
-            return (helpLevel > 0) ? 0 : 1;
-        }
-        
-        cout << "Using flag-based configuration:" << endl;
-        if (configParams.batchMode) {
-            cout << "Input directory: " << configParams.rawTPX3Folder << endl;
-            cout << "Batch mode: ALL files" << endl;
-        } else {
-            cout << "Input file: " << configParams.rawTPX3Folder << "/" << configParams.rawTPX3File << endl;
-        }
-        cout << "Output folder: " << configParams.outputFolder << endl;
-        cout << "Verbose level: " << configParams.verboseLevel << endl;
-    }
-    else {
-        cerr << "Error: Unrecognized option. Please use flags starting with '-' or '--'." << endl;
-        cerr << "Provided: " << firstArg << endl;
-        cout << "Note: To use a config file, use: -c <config_file>" << endl;
-        printUsage(argv[0]);
-        return 1;
+    std::ifstream input(argv[1], std::ios::binary);
+    if (!input) {
+        std::cerr << "Unable to open TPX3 input file: " << argv[1] << '\n';
+        return 2;
     }
 
-    processTPX3Files(configParams);
+    const auto result = hermes_tpx3_spidr::unpack(input);
+    hermes_tpx3_spidr::printSummary(result.summary, std::cout);
+    hermes_tpx3_spidr::printMessages(result.summary, std::cerr);
 
-	return 0;
+    return result.summary.errors.empty() ? 0 : 1;
 }
