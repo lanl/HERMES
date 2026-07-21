7248825301754b9367a14c3ca91b04befa26568d
Alexander M. Long <alexlong@lanl.gov>
2026-07-21 16:56:50 -0600

    Remove OutputRows struct definition from unpacker.h

--- backends/unpackers/tpx3-spidr/cpp/inc/unpacker.h
+++ backends/unpackers/tpx3-spidr/cpp/inc/unpacker.h
@@ -21,14 +21,6 @@ void unpackPacket(std::uint64_t raw_word,
 
 UnpackResult unpack(std::istream& input);
 
-struct OutputRows {
-    std::vector<PixelOutputRow> pixels;
-    std::vector<TdcOutputRow> tdcs;
-    std::vector<GlobalOutputRow> globals;
-    std::vector<ControlOutputRow> controls;
-    std::vector<UnknownOutputRow> unknowns;
-};
-
 struct WorkflowResult {
     bool success = false;
     std::string output_directory;
