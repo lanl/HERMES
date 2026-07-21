b941759f040c46e022cca43732595a903a1c6705
Alexander M. Long <alexlong@lanl.gov>
2026-07-21 15:29:11 -0600

    Add OutputRows and WorkflowResult structures to unpacker header

--- backends/unpackers/tpx3-spidr/cpp/inc/unpacker.h
+++ backends/unpackers/tpx3-spidr/cpp/inc/unpacker.h
@@ -2,10 +2,14 @@
 #define HERMES_TPX3_SPIDR_UNPACKER_H
 
 #include "packet_types.h"
+#include "output_rows.h"
+#include "summary_json.h"
 
 #include <cstddef>
 #include <cstdint>
 #include <istream>
+#include <string>
+#include <vector>
 
 namespace hermes_tpx3_spidr {
 
@@ -16,6 +20,24 @@ void unpackPacket(std::uint64_t raw_word,
                   UnpackResult& result);
 
 UnpackResult unpack(std::istream& input);
+
+struct OutputRows {
+    std::vector<PixelOutputRow> pixels;
+    std::vector<TdcOutputRow> tdcs;
+    std::vector<GlobalOutputRow> globals;
+    std::vector<ControlOutputRow> controls;
+    std::vector<UnknownOutputRow> unknowns;
+};
+
+struct WorkflowResult {
+    bool success = false;
+    std::string output_directory;
+    SummaryJsonContent summary;
+    std::vector<std::string> errors;
+};
+
+WorkflowResult runTwoPassWorkflow(std::istream& input,
+                                  const std::string& output_directory);
 
 }  // namespace hermes_tpx3_spidr
 
